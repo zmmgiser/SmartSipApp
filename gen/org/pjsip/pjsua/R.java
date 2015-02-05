dac50c81e491c5a7e62b73b6ee0933a07e970e27
yrc <iponkan@gmail.com>
2015-02-05 10:45:36 +0800

    have to use eclipse

--- gen/org/pjsip/pjsua/R.java
+++ gen/org/pjsip/pjsua/R.java
@@ -10,32 +10,34 @@ package org.pjsip.pjsua;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int loadBackground=0x7f040000;
+    }
     public static final class drawable {
-        public static final int account_edit=0x7f020000;
-        public static final int main_image=0x7f020001;
-        public static final int splash_screen=0x7f020002;
+        public static final int main_image=0x7f020000;
+        public static final int splash_screen=0x7f020001;
     }
     public static final class id {
-        public static final int button_calllist_hangup=0x7f060011;
-        public static final int button_destroy=0x7f060000;
-        public static final int button_hangupall=0x7f06000f;
-        public static final int button_login=0x7f060002;
-        public static final int button_logoff=0x7f060003;
-        public static final int button_makecall=0x7f060006;
-        public static final int button_makecall2=0x7f06000c;
-        public static final int button_options=0x7f060008;
-        public static final int button_sendim=0x7f060007;
-        public static final int edittext_callurl=0x7f06000b;
-        public static final int edittext_callurl2=0x7f06000e;
-        public static final int listview_calllist=0x7f060005;
-        public static final int textview_call_state=0x7f060004;
-        public static final int textview_call_url_end=0x7f06000a;
-        public static final int textview_call_url_start=0x7f060009;
-        public static final int textview_call_url_start2=0x7f06000d;
-        public static final int textview_calllist_name=0x7f060012;
-        public static final int textview_calllist_state=0x7f060013;
-        public static final int textview_register_state=0x7f060001;
-        public static final int video_view=0x7f060010;
+        public static final int button_calllist_hangup=0x7f070011;
+        public static final int button_destroy=0x7f070000;
+        public static final int button_hangupall=0x7f07000f;
+        public static final int button_login=0x7f070002;
+        public static final int button_logoff=0x7f070003;
+        public static final int button_makecall=0x7f070006;
+        public static final int button_makecall2=0x7f07000c;
+        public static final int button_options=0x7f070008;
+        public static final int button_sendim=0x7f070007;
+        public static final int edittext_callurl=0x7f07000b;
+        public static final int edittext_callurl2=0x7f07000e;
+        public static final int listview_calllist=0x7f070005;
+        public static final int textview_call_state=0x7f070004;
+        public static final int textview_call_url_end=0x7f07000a;
+        public static final int textview_call_url_start=0x7f070009;
+        public static final int textview_call_url_start2=0x7f07000d;
+        public static final int textview_calllist_name=0x7f070012;
+        public static final int textview_calllist_state=0x7f070013;
+        public static final int textview_register_state=0x7f070001;
+        public static final int video_view=0x7f070010;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -43,10 +45,10 @@ public final class R {
         public static final int call_list_adapter=0x7f030002;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int title_activity_main=0x7f040001;
+        public static final int app_name=0x7f050000;
+        public static final int title_activity_main=0x7f050001;
     }
     public static final class style {
-        public static final int AppTheme=0x7f050000;
+        public static final int AppTheme=0x7f060000;
     }
 }
